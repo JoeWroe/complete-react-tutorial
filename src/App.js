9e43dbbc85870162fc9b3193e74a56c25148280f
Joe Wroe <joe_wroe@icloud.com>
2018-11-22 23:49:32 +0000

    Setup basic state for my first component
    Joe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app-content">
-        <h1>Hello, World!</h1>
-        <p>{ Math.random() * 10 }</p>
-      </div>
-    );
-  }
+    state = {
+        name: 'Ryu',
+        age: 30
+    }
+
+    render() {
+        return (
+            <div className="app-content">
+                <h1>Hello, World!</h1>
+                <p>My name is: { this.state.name }</p>
+                <p>I am: { this.state.age }</p>
+            </div>
+        );
+    }
 }
 
 export default App;
