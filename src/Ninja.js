19285658ed896bb1ff62b7cfbbd370b3f3ce3463
Joe Wroe <joe_wroe@icloud.com>
2018-12-03 17:56:30 +0000

    Setup a conditional output for the Ninja component
    Joe

--- src/Ninja.js
+++ src/Ninja.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const Ninja = ({ ninjas }) => {
+const Ninja = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ninja.age >= 20 ? (
             <div className='ninja' key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div>
+                <br/>
+            </div>
+        ) : (
+            <div className='redacted'>
+                <div>**PARENTAL SUPERVISION REQUIRED**</div>
                 <br/>
             </div>
         )
